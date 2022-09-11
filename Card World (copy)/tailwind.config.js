8374a8521072229bdd484c8663b849ef353a05ed
Summsitup.eth <93762798+plsummers@users.noreply.github.com>
2022-09-11 18:14:42 -0500

    upload
    site

---
+++ Card World (copy)/tailwind.config.js
@@ -0,0 +1,147 @@
+module.exports = {
+    "mode": "jit",
+    "purge": [
+        ".\/**\/*.html"
+    ],
+    "darkMode": false,
+    "theme": {
+        "container": {
+            "center": true,
+            "padding": {
+                "DEFAULT": "1rem",
+                "sm": "2rem",
+                "lg": "4rem",
+                "xl": "5rem",
+                "2xl": "6rem"
+            }
+        },
+        "extend": {
+            "fontFamily": {
+                "title": [
+                    "Rubik"
+                ],
+                "body": [
+                    "Roboto"
+                ]
+            },
+            "borderRadius": {
+                "main": "0.75rem",
+                "button": "0.75rem"
+            },
+            "boxShadow": {
+                "main": "0 25px 50px -12px rgba(0, 0, 0, 0.25)"
+            },
+            "dropShadow": {
+                "main": "0 25px 25px rgba(0, 0, 0, 0.15)"
+            },
+            "backgroundImage": {
+                "body-image": "url('https:\/\/cookgroup.tools\/files\/projects\/yOYg932K3N2kQvPL\/assets\/cardworldlogotransparentpng.png')"
+            },
+            "backgroundPosition": {
+                "body-position": "center"
+            },
+            "backgroundSize": {
+                "body-size": "cover"
+            },
+            "colors": {
+                "body": "#220425",
+                "header": "#D46DE6",
+                "content": "#FFE3FF",
+                "primary": {
+                    "button": {
+                        "text": "#FFFFFF"
+                    },
+                    "DEFAULT": "#CE75C6",
+                    "100": "#E3B0DE",
+                    "200": "#DEA6D9",
+                    "300": "#DA99D4",
+                    "400": "#D68CCF",
+                    "500": "#CE75C6",
+                    "600": "#C55EBC",
+                    "700": "#C152B7",
+                    "800": "#BD45B2",
+                    "900": "#B240A8"
+                },
+                "secondary": {
+                    "button": {
+                        "text": "#FFFFFF"
+                    },
+                    "DEFAULT": "#D27CD6",
+                    "100": "#E8BDEA",
+                    "200": "#E3B0E6",
+                    "300": "#DFA3E2",
+                    "400": "#DA96DE",
+                    "500": "#D27CD6",
+                    "600": "#C963CD",
+                    "700": "#C454C9",
+                    "800": "#C047C5",
+                    "900": "#BB3DC1"
+                },
+                "tertiary": {
+                    "DEFAULT": "#E3B5E8",
+                    "100": "#FFFFFF",
+                    "200": "#FAF5FB",
+                    "300": "#F4E6F6",
+                    "400": "#EED4F1",
+                    "500": "#E3B5E8",
+                    "600": "#D796DE",
+                    "700": "#D185D9",
+                    "800": "#CB75D4",
+                    "900": "#C566CF"
+                },
+                "izLXwTuR": {
+                    "link": "#FFFFFF",
+                    "link-hover": "#FFFFFF",
+                    "mobile-link": "#FFFFFF"
+                },
+                "TAXUdhZUvw8Qp7tOgh3k": {
+                    "body": "#FFFAFA",
+                    "header": "#D27CD6",
+                    "section-background": "#220425"
+                },
+                "t2hYzSQS": {
+                    "body": "#FFFFFF",
+                    "header": "#FFFFFF",
+                    "feature-background": "#B166AA",
+                    "section-background": "#220425"
+                },
+                "0JZnft6S": {
+                    "body": "#FFFFFF",
+                    "title": "#D889D0",
+                    "border": "#BDBDBD",
+                    "feature": "#FFFFFF",
+                    "background": "#4D3250",
+                    "price-plan": "#FFFAFA",
+                    "section-background": "#220425"
+                },
+                "TqoVE6HB": {
+                    "body": "#7A4075",
+                    "header": "#FFFFFF",
+                    "background": "#4D3250"
+                },
+                "YVBTqAUjdYN8LPjROzFV": {
+                    "link": "#FFFFFF",
+                    "background": "#220425",
+                    "link-hover": "#E3B5E8"
+                },
+                "cHa6qCaK": {
+                    "link": "#FFFFFF",
+                    "link-hover": "#eeeeee",
+                    "mobile-link": "#FFFFFF",
+                    "mobile-link-hover": "#FFFFFF"
+                },
+                "9GL2vDAE": {
+                    "body": "#FFFFFF",
+                    "title": "#E29CDA",
+                    "border": "#220425",
+                    "price-plan": "#bdbdbd"
+                },
+                "3eDga0bC": {
+                    "link": "#FFFFFF",
+                    "link-hover": "#eeeeee"
+                }
+            }
+        }
+    },
+    "plugins": []
+}
